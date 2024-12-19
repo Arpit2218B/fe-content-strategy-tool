70433bbf958adb941562a5703d7a19947c291cac
Arpit Rathi <arpit97rathi@gmail.com>
2024-12-19 18:36:55 +0530

    fix: remove react dev tools use

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,7 @@
-import { compose, createStore } from 'redux';
+import { createStore } from 'redux';
 import { reducer } from './reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { config } from '@/utils/config';
 
 const store = createStore(
   reducer,
-  config.ENVIRONMENT === 'local' ? composeWithDevTools() : compose(),
 );
 export default store;
